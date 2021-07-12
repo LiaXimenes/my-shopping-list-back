0afe242e7e32f2091f3c58a5c0e7a1149c3451d0
Lia Ximenes <ximeneslia@gmail.com>
2021-07-12 15:35:54 -0300

    implementação das rotas

--- src/app.js
+++ src/app.js
@@ -7,4 +7,38 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post("/items", async (req, res) => {
+    const {text} = req.body
+
+    try{
+        if(text !== ""){
+            await connection.query(`
+            INSERT INTO items (text)
+            VALUES ($1)`, [text]);
+
+            res.sendStatus(201);
+        } else{
+            res.sendStatus(400);
+        }
+
+    } catch{
+        res.sendStatus(500);
+    }
+})
+
+app.get("/items", async (req, res) => {
+
+    try{
+        const result = await connection.query(`
+        SELECT * FROM items
+        `);
+
+        res.send(result.rows);
+    
+    } catch{
+        res.sendStatus(500);
+
+    }
+})
+
 export default app;
